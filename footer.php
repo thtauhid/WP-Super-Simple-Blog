570c26aa42cc2ca8dda91319bfd3d1563bf9832b
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-06-05 01:52:59 +0600

    Dynamic site name in copyright.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<footer>
 			<p class="text-center footer-text">
-				Tauhid's &copy; 2018. All rights reserved.
+			&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ) ?> | <?php echo esc_html( __('Powered by', 'moduler') ); ?> <a href="https://wordpress.org"><?php echo esc_html( __('Wordpress', 'moduler') ); ?></a>
 			</p>
 		</footer>
     </div>
