c2546ce39e16f08f87415d98466f89e638b9e82c
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-06-05 01:00:09 +0600

    Dynamic post.

--- index.php
+++ index.php
@@ -5,83 +5,48 @@
 				<!--Blog Update-->
                 <div class="row">
 
+
+    <?php if(have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+
+
 					<!--Blog Update 01-->
                     <div class="col-12">
-                        <div class="card mb-4"> <img class="img-thumbnail" src="<?php echo esc_html( get_template_directory_uri() . '/assets/img/binder-blank-book-bindings-479815.jpg'  ); ?> ">
+                        <div class="card mb-4"> <img class="img-thumbnail" src="<?php 
+      if(has_post_thumbnail()) {the_post_thumbnail_url();} else{ echo esc_html( get_template_directory_uri() . '/assets/img/binder-blank-book-bindings-479815.jpg'  ); } ?> ">
                             <br>
                             <div class="card-body">
-                                <h3 class="card-title"><a href="#">Card title</a></h3>
-                                <p>On 12 May, 2018 by Tauhid</p>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <button type="button" class="btn btn-outline-primary">More >></button>
+                                <h3 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+                                <p><?php echo esc_html( __('On ', 'moduler') ); the_time('j F, Y');  echo esc_html( __(' by ', 'moduler') ); the_author_posts_link(); ?></p>
+                                <p class="card-text"><?php the_excerpt() ?></p>
+                                <a href="<?php the_permalink() ?>"><?php echo esc_html( __('More >>', 'moduler') ); ?></a><br>
                             </div>
                         </div>
                         <hr> <hr>  <br> 
 					</div>
 					
-
-					<!--Blog Update 02-->
-					<div class="col-12">
-							<div class="card mb-4"> <img class="img-thumbnail" src="<?php echo esc_html( get_template_directory_uri() . '/assets/img/binder-blank-book-bindings-479815.jpg'  ); ?>">
-								<br>
-								<div class="card-body">
-									<h3 class="card-title"><a href="#">Card title</a></h3>
-									<p>On 12 May, 2018 by Tauhid</p>
-									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-									<button type="button" class="btn btn-outline-primary">More >></button>
-								</div>
-							</div>
-							<hr> <hr>  <br> 
-						</div>
+                    <?php endwhile; ?>
 
 
 
-											<!--Blog Update 03-->
-					<div class="col-12">
-							<div class="card mb-4"> <img class="img-thumbnail" src="<?php echo esc_html( get_template_directory_uri() . '/assets/img/binder-blank-book-bindings-479815.jpg'  ); ?>">
-								<br>
-								<div class="card-body">
-									<h3 class="card-title"><a href="#">Card title</a></h3>
-									<p>On 12 May, 2018 by Tauhid</p>
-									<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-									<button type="button" class="btn btn-outline-primary">More >></button>
-								</div>
-							</div>
-						</div>
 
-
-
-				</div>
-				<!--Blog Pagination-->
-				<div class="row">
-					<div class="col-12">
-							<div>
-									<ul class="pagination">
-									  <li class="page-item disabled">
-										<a class="page-link" href="#">&laquo;</a>
-									  </li>
-									  <li class="page-item active">
-										<a class="page-link" href="#">1</a>
-									  </li>
-									  <li class="page-item">
-										<a class="page-link" href="#">2</a>
-									  </li>
-									  <li class="page-item">
-										<a class="page-link" href="#">3</a>
-									  </li>
-									  <li class="page-item">
-										<a class="page-link" href="#">4</a>
-									  </li>
-									  <li class="page-item">
-										<a class="page-link" href="#">5</a>
-									  </li>
-									  <li class="page-item">
-										<a class="page-link" href="#">&raquo;</a>
-									  </li>
-									</ul>
-								  </div>
-					</div>
 				</div>
             </div>
-		</div>
+        </div>
+        
+        <div class="row">
+    <div class="col-8">
+      <div class="text-right">
+        <?php previous_posts_link(__('Newer>>', 'moduler')); ?>
+      </div>
+      <div class="text-left">
+        <?php next_posts_link(__('<<Older', 'moduler')); ?>
+      </div>
+    </div>
+  </div>
+        <?php else : ?>
+      <p><?php echo esc_html( __('No Posts Found', 'moduler') ); ?></p>
+    <?php endif; ?>
+
+
 <?php get_footer(); ?>
